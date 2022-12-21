4bd3f1eb68fc8ed537ae08de2f2596ccead8ee42
dominik.listos <dominik.listos@mobica.com>
2022-12-21 10:49:13 +0100

    two text fields with a button

--- app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -1,17 +1,18 @@
 package com.example.jetnote.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,31 +20,59 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetnote.R
+import com.example.jetnote.components.NoteButton
+import com.example.jetnote.components.NoteInputText
 
 @Composable
 fun NoteScreen() {
     Column(modifier = Modifier.padding(6.dp)) {
-        TopAppBar(
-            title = {
-                Text(text = stringResource(id = R.string.app_name))
-            },
-            actions = {
-                Icon(
-                    imageVector = Icons.Rounded.Notifications,
-                    contentDescription = "Rounded icon"
-                )
-            },
-            backgroundColor = Color.LightGray
-        )
 
-        // Content
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
+        var title by remember {
+            mutableStateOf("")
+        }
+        var description by remember {
+            mutableStateOf("")
         }
 
+        TopAppBar(title = {
+            Text(text = stringResource(id = R.string.app_name))
+        }, actions = {
+            Icon(
+                imageVector = Icons.Rounded.Notifications, contentDescription = "Rounded icon"
+            )
+        }, backgroundColor = Color.LightGray
+        )
+        // Content
+        Column(
+            modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NoteInputText(
+                modifier = Modifier.padding(
+                    top = 9.dp,
+                    bottom = 8.dp
+                ),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) title = it
+                }
+            )
+            NoteInputText(
+                modifier = Modifier.padding(
+                    top = 9.dp,
+                    bottom = 8.dp
+                ),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) description = it
+                }
+            )
+            NoteButton(
+                text = "Save",
+                onClick = {}
+            )
+        }
     }
 }
 
